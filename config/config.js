b8029da0234abd28452858d0d870c188dec9a566
William Hubenschmidt <whubenschmidt@gmail.com>
2019-10-06 00:59:46 -0400

    Update config.js

--- config/config.js
+++ config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DEVELOPMENT_DB_USERNAME,
-    password: process.env.DEVELOPMENT_DB_USERNAME,
+    password: process.env.DEVELOPMENT_DB_PASSWORD,
     database: process.env.DEVELOPMENT_DB_DATABASE,
     host: process.env.DEVELOPMENT_DB_HOST,
     dialect: "postgres",
