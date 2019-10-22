ea55b786a7208aeeef867b58a3ccf90bb37a395f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-10-22 19:54:35 +0000

    Deploy hubenschmidt/cra-express-travis-ci to github.com/hubenschmidt/cra-express-travis-ci.git:gh-pages

---
+++ precache-manifest.2196ee4e20f0a30ffc247854d2a44f4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2844f6141711483af923a087168dd57f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c7157d8fdd51ef8cde8a",
+    "url": "/static/css/main.b80217f8.chunk.css"
+  },
+  {
+    "revision": "f9685dc9e821160aa52e",
+    "url": "/static/js/2.3053b6a4.chunk.js"
+  },
+  {
+    "revision": "c7157d8fdd51ef8cde8a",
+    "url": "/static/js/main.ffd33b3d.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "495d93cc2c05774c4e88882137125113",
+    "url": "/static/media/logo.495d93cc.svg"
+  }
+]);
